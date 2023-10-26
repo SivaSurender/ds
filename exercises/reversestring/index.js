8a59a781576e1e20a929439504da04f590ab9a7e
SivaSurender <e.surender14@gmail.com>
2023-10-26 19:59:37 +0530

    Added code

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,32 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse(str) {
+//   return str.split("").reverse().join("");
+// }
+// function reverse(str) {
+//   let rev = "";
+
+//   for (let i = str.length; i >= 0; i--) {
+//     rev = rev + str.charAt(i);
+//   }
+
+//   return rev;
+// }
+function reverse(str) {
+  return str.split("").reduce((acc, curr) => curr + acc, "");
+}
+
+// const test = "car";
+// let fdsf = "";
+// for (let char of test) {
+//   console.log(char);
+//   fdsf = char + fdsf;
+// }
+// let test = "";
+
+// test = "c" + test;
+// test = "t" + test;
+// console.log(test);
 
 module.exports = reverse;
