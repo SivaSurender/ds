2e4a8d0213646eb5e2d6ecacbb1790964163d460
Bala <bala.e@Balas-MacBook-Air.local>
2023-10-26 22:31:18 +0530

    palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -8,15 +8,35 @@
 //   palindrome("abcdefg") === false
 
 // 1
+// function palindrome(str) {
+//   let initial = str;
+//   let final = "";
+
+//   for (i = str.length; i >= 0; i--) {
+//     final += str.charAt(i);
+//   }
+
+//   return initial === final;
+// }
+
+//2
+
+// function palindrome(str) {
+//   let init = str.split("").reverse().join("");
+
+//   return init === str;
+// }
+
+//3
+// function palindrome(str) {
+//   return str.charAt(0) === str.charAt(str.length - 1);
+// }
+
+//4
 function palindrome(str) {
-  let initial = str;
-  let final = "";
-
-  for (i = str.length; i >= 0; i--) {
-    final += str.charAt(i);
-  }
-
-  return initial === final;
+  return str.split("").every((item, index) => {
+    return item === str[str.length - index - 1];
+  });
 }
 
 module.exports = palindrome;
