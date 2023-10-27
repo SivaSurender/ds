516a91ec3007f8c119ef2ece6c2fda3e0000cc1e
Bala <bala.e@Balas-MacBook-Air.local>
2023-10-27 20:49:40 +0530

    Added code

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,16 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// approach 1
+function reverseInt(n) {
+  let change = String(n).split("");
+
+  let id = change.includes("-")
+    ? change.filter((each) => each !== "-")
+    : change;
+  return change.includes("-")
+    ? Number("-" + id.reverse().join(""))
+    : Number(id.reverse().join(""));
+}
 
 module.exports = reverseInt;
