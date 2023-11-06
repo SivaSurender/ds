0456164463846ba254ea77c7ecd8fc418d3c77a6
Bala <bala.e@Balas-MacBook-Air.local>
2023-11-06 21:58:07 +0530

    anagram

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,41 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  const countChar = (str) => {
+    return str
+      .toLowerCase()
+      .replace(/[^a-z]/g, "")
+      .split("")
+      .reduce((charCount, char) => {
+        if (charCount[char]) {
+          // console.log(charCount[char])
+          charCount[char]++;
+        } else {
+          charCount[char] = 1;
+        }
+        return charCount;
+      }, {});
+  };
+
+  const charCountA = countChar(stringA);
+  const charCountB = countChar(stringB);
+
+  console.log({ charCountA, charCountB });
+
+  if (Object.keys(charCountA).length !== Object.keys(charCountB).length) {
+    return false;
+  }
+
+  for (let char in charCountA) {
+    if (charCountA[char] !== charCountB[char]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+anagrams("rail safety", "fairy tales");
 
 module.exports = anagrams;
